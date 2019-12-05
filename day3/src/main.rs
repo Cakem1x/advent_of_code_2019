58744760282d5e78f7265bf10cc03b923dc36920
Cakemix <mholoch@gmail.com>
2019-12-05 11:51:39 +0100

    finished day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,8 +6,20 @@ fn main() {
     let wire_strings: Vec<&str> = input_string.split("\n").collect();
     let wire1 = wire_from_string(&wire_strings[0]);
     let wire2 = wire_from_string(&wire_strings[1]);
-    let closest_intersection = get_closest_intersection(&wire1, &wire2).expect("No interesction found!");
-    println!("Closest intersection point is {:?} with distance {}", closest_intersection.0, closest_intersection.1);
+    // day3 part1
+    let closest_intersection =
+        get_closest_intersection(&wire1, &wire2).expect("No interesction found!");
+    println!(
+        "Closest intersection point is {:?} with distance {}",
+        closest_intersection.0, closest_intersection.1
+    );
+    // day3 part2
+    let closest_intersection =
+        get_closest_intersection_by_steps(&wire1, &wire2).expect("No interesction found!");
+    println!(
+        "Closest intersection point by steps is {:?} with nr of steps {}",
+        closest_intersection.0, closest_intersection.1
+    );
 }
 
 #[derive(Debug, PartialEq)]
@@ -81,6 +93,24 @@ impl GridLine {
     fn is_vertical(&self) -> bool {
         return self.0.x == self.1.x;
     }
+
+    fn contains(&self, p: &Point) -> bool {
+        if self.is_horizontal() {
+            self.0.y == p.y
+                && ((self.0.x..self.1.x + 1).contains(&p.x)
+                    || (self.1.x..self.0.x + 1).contains(&p.x))
+        } else if self.is_vertical() {
+            self.0.x == p.x
+                && ((self.0.y..self.1.y + 1).contains(&p.y)
+                    || (self.1.y..self.0.y + 1).contains(&p.y))
+        } else {
+            panic!("{:?} is not a GridLine!", self);
+        }
+    }
+
+    fn len(&self) -> u32 {
+        return manhattan_distance(&self.0, &self.1);
+    }
 }
 
 fn wire_from_string(string: &str) -> Vec<GridLine> {
@@ -120,18 +150,57 @@ fn wire_from_string(string: &str) -> Vec<GridLine> {
 }
 
 fn intersect_wires(wire1: &Vec<GridLine>, wire2: &Vec<GridLine>) -> Vec<Point> {
-    let origin = Point{x:0, y:0};
+    let origin = Point { x: 0, y: 0 };
     let mut intersection_points = Vec::new();
     // don't intersect the first lines, otherwise the origin will be an intersection point
     for line1 in &wire1[..] {
         for line2 in &wire2[..] {
             match line1.intersect(line2) {
-                Some(intersection_point) => if intersection_point != origin {intersection_points.push(intersection_point)} else {()},
+                Some(intersection_point) => {
+                    if intersection_point != origin {
+                        intersection_points.push(intersection_point)
+                    } else {
+                        ()
+                    }
+                }
                 None => (),
             }
         }
     }
     return intersection_points;
+}
+
+fn wire_steps_to_point(wire: &Vec<GridLine>, point: &Point) -> Option<u32> {
+    let mut steps = 0;
+    for l in wire {
+        if l.contains(&point) {
+            return Some(steps + manhattan_distance(&l.0, point));
+        } else {
+            steps += l.len();
+        }
+    }
+    return None;
+}
+
+fn get_closest_intersection_by_steps(
+    wire1: &Vec<GridLine>,
+    wire2: &Vec<GridLine>,
+) -> Option<(Point, u32)> {
+    let intersection_points = intersect_wires(wire1, wire2);
+    let mut closest_intersection_point = None;
+    let mut closest_intersection_distance = u32::max_value();
+    for point in intersection_points {
+        let steps = wire_steps_to_point(&wire1, &point).expect("Intersection point not on wire1!")
+            + wire_steps_to_point(&wire2, &point).expect("Intersection point not on wire1!");
+        if steps < closest_intersection_distance {
+            closest_intersection_distance = steps;
+            closest_intersection_point = Some(point);
+        }
+    }
+    match closest_intersection_point {
+        None => return None,
+        Some(point) => return Some((point, closest_intersection_distance)),
+    }
 }
 
 fn get_closest_intersection(wire1: &Vec<GridLine>, wire2: &Vec<GridLine>) -> Option<(Point, u32)> {
@@ -199,6 +268,50 @@ fn test_grid_line_intersection() {
         l1.intersect(&l2).expect("no intersection!"),
         Point { x: 0, y: 10 }
     );
+}
+
+#[test]
+fn test_grid_line_contains_self() {
+    let l = GridLine(Point { x: 12, y: 17 }, Point { x: 12, y: 20 });
+    assert_eq!(l.contains(&l.0), true);
+    assert_eq!(l.contains(&l.1), true);
+}
+
+#[test]
+#[should_panic]
+fn test_grid_line_contains_panics() {
+    let l = GridLine(Point { x: 10, y: 17 }, Point { x: 12, y: 20 });
+    assert_eq!(l.contains(&l.0), true);
+}
+
+#[test]
+fn test_grid_line_horizontal_contains() {
+    let l = GridLine(Point { x: -12, y: 1 }, Point { x: 12, y: 1 });
+    assert_eq!(l.contains(&Point { x: 10, y: 2 }), false);
+    assert_eq!(l.contains(&Point { x: 0, y: 1 }), true);
+}
+
+#[test]
+fn test_grid_line_vertical_contains() {
+    let l = GridLine(Point { x: 12, y: 17 }, Point { x: 12, y: 20 });
+    assert_eq!(l.contains(&Point { x: 10, y: 18 }), false);
+    assert_eq!(l.contains(&Point { x: 12, y: 18 }), true);
+}
+
+#[test]
+fn test_wire_intersection_distance_by_steps1() {
+    let wire1 = wire_from_string("R75,D30,R83,U83,L12,D49,R71,U7,L72");
+    let wire2 = wire_from_string("U62,R66,U55,R34,D71,R55,D58,R83");
+    let closest_intersection = get_closest_intersection_by_steps(&wire1, &wire2);
+    assert_eq!(closest_intersection.expect("No intersection found!").1, 610);
+}
+
+#[test]
+fn test_wire_intersection_distance_by_steps2() {
+    let wire1 = wire_from_string("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+    let wire2 = wire_from_string("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+    let closest_intersection = get_closest_intersection_by_steps(&wire1, &wire2);
+    assert_eq!(closest_intersection.expect("No intersection found!").1, 410);
 }
 
 #[test]
