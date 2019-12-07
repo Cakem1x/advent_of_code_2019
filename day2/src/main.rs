11b48567db82eea41efcff8caadf06e9a503768d
Cakemix <mholoch@gmail.com>
2019-12-07 22:10:02 +0100

    added test case for day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -41,7 +41,11 @@ fn main() {
 
 fn run_program(state: &mut [u32]) {
     let mut instruction_pointer = 0;
-    while !step_program(instruction_pointer, state) {
+    loop {
+        let terminate = step_program(instruction_pointer, state);
+        if terminate {
+            break;
+        }
         instruction_pointer += 4;
     }
 }
@@ -109,4 +113,11 @@ fn test_run_program() {
     let mut program_state = [1, 1, 1, 4, 99, 5, 6, 0, 99];
     run_program(&mut program_state);
     assert_eq!(program_state, [30, 1, 1, 4, 2, 5, 6, 0, 99]);
+}
+
+#[test]
+fn test_day2_works() {
+    let mut program_state = [1, 53, 79, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 6, 19, 1, 9, 19, 23, 1, 6, 23, 27, 1, 10, 27, 31, 1, 5, 31, 35, 2, 6, 35, 39, 1, 5, 39, 43, 1, 5, 43, 47, 2, 47, 6, 51, 1, 51, 5, 55, 1, 13, 55, 59, 2, 9, 59, 63, 1, 5, 63, 67, 2, 67, 9, 71, 1, 5, 71, 75, 2, 10, 75, 79, 1, 6, 79, 83, 1, 13, 83, 87, 1, 10, 87, 91, 1, 91, 5, 95, 2, 95, 10, 99, 2, 9, 99, 103, 1, 103, 6, 107, 1, 107, 10, 111, 2, 111, 10, 115, 1, 115, 6, 119, 2, 119, 9, 123, 1, 123, 6, 127, 2, 127, 10, 131, 1, 131, 6, 135, 2, 6, 135, 139, 1, 139, 5, 143, 1, 9, 143, 147, 1, 13, 147, 151, 1, 2, 151, 155, 1, 10, 155, 0, 99, 2, 14, 0, 0];
+    run_program(&mut program_state);
+    assert_eq!(program_state[0], 19690720);
 }
