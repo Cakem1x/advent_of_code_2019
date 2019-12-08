525bd430798ecbeddc7d8a711e7a64565ae3bc7b
Cakemix <mholoch@gmail.com>
2019-12-08 15:16:34 +0100

    finished day5 part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -73,6 +73,10 @@ enum Opcode {
     Mul,
     Input,
     Output,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
     Terminate,
 }
 
@@ -88,6 +92,10 @@ fn parse_instruction(opcode_int: i32) -> (Opcode, ParameterMode, ParameterMode, 
         2 => Opcode::Mul,
         3 => Opcode::Input,
         4 => Opcode::Output,
+        5 => Opcode::JumpIfTrue,
+        6 => Opcode::JumpIfFalse,
+        7 => Opcode::LessThan,
+        8 => Opcode::Equals,
         99 => Opcode::Terminate,
         x => panic!("Invalid opcode found: {}!", x),
     };
@@ -180,10 +188,167 @@ fn step_program(instruction_pointer: usize, state: &mut [i32]) -> (usize, Option
             };
             return (instruction_pointer + 2, Some(output_value));
         }
+        (Opcode::JumpIfTrue, pm1, pm2, _pm3) => {
+            let condition_param = state[instruction_pointer + 1];
+            let condition_value = match pm1 {
+                ParameterMode::Position => state[condition_param as usize],
+                ParameterMode::Immediate => condition_param,
+            };
+            let jump_target_param = state[instruction_pointer + 2];
+            let jump_target_value = match pm2 {
+                ParameterMode::Position => state[jump_target_param as usize] as usize,
+                ParameterMode::Immediate => jump_target_param as usize,
+            };
+            if condition_value != 0 {
+                return (jump_target_value, None);
+            } else {
+                return (instruction_pointer + 3, None);
+            }
+        }
+        (Opcode::JumpIfFalse, pm1, pm2, _pm3) => {
+            let condition_param = state[instruction_pointer + 1];
+            let condition_value = match pm1 {
+                ParameterMode::Position => state[condition_param as usize],
+                ParameterMode::Immediate => condition_param,
+            };
+            let jump_target_param = state[instruction_pointer + 2];
+            let jump_target_value = match pm2 {
+                ParameterMode::Position => state[jump_target_param as usize] as usize,
+                ParameterMode::Immediate => jump_target_param as usize,
+            };
+            if condition_value == 0 {
+                return (jump_target_value, None);
+            } else {
+                return (instruction_pointer + 3, None);
+            }
+        }
+        (Opcode::LessThan, pm1, pm2, pm3) => {
+            let first_operand_param = state[instruction_pointer + 1];
+            let first_operand_value = match pm1 {
+                ParameterMode::Position => state[first_operand_param as usize],
+                ParameterMode::Immediate => first_operand_param,
+            };
+            let second_operand_param = state[instruction_pointer + 2];
+            let second_operand_value = match pm2 {
+                ParameterMode::Position => state[second_operand_param as usize],
+                ParameterMode::Immediate => second_operand_param,
+            };
+            let result_param = state[instruction_pointer + 3];
+            let result_ptr = match pm3 {
+                ParameterMode::Position => result_param as usize,
+                _ => panic!("at &{} -> {}: result parameter only supports position mode.", instruction_pointer, state[instruction_pointer]),
+            };
+            if first_operand_value < second_operand_value {
+                state[result_ptr] = 1;
+            } else {
+                state[result_ptr] = 0;
+            }
+            return (instruction_pointer + 4, None);
+        }
+        (Opcode::Equals, pm1, pm2, pm3) => {
+            let first_operand_param = state[instruction_pointer + 1];
+            let first_operand_value = match pm1 {
+                ParameterMode::Position => state[first_operand_param as usize],
+                ParameterMode::Immediate => first_operand_param,
+            };
+            let second_operand_param = state[instruction_pointer + 2];
+            let second_operand_value = match pm2 {
+                ParameterMode::Position => state[second_operand_param as usize],
+                ParameterMode::Immediate => second_operand_param,
+            };
+            let result_param = state[instruction_pointer + 3];
+            let result_ptr = match pm3 {
+                ParameterMode::Position => result_param as usize,
+                _ => panic!("at &{} -> {}: result parameter only supports position mode.", instruction_pointer, state[instruction_pointer]),
+            };
+            if first_operand_value == second_operand_value {
+                state[result_ptr] = 1;
+            } else {
+                state[result_ptr] = 0;
+            }
+            return (instruction_pointer + 4, None);
+        }
         (Opcode::Terminate, _pm1, _pm2, _pm3) => {
             return (instruction_pointer, None);
         }
+        //instruction => panic!("opcode {:?} not supported!", instruction.0),
     }
+}
+
+#[test]
+fn program_with_less_than_in_immediate_mode() {
+    for input in 0..8 {
+        let mut program_state = [1107,input,8,1,4,1,99];
+        run_program(&mut program_state);
+        assert_eq!(program_state[1], 1);
+    }
+    for input in 8..12 {
+        let mut program_state = [1107,input,8,1,4,1,99];
+        run_program(&mut program_state);
+        assert_eq!(program_state[1], 0);
+    }
+}
+
+#[test]
+fn program_with_equals_in_immediate_mode() {
+    for input in 0..8 {
+        let mut program_state = [1108,input,8,1,4,1,99];
+        run_program(&mut program_state);
+        assert_eq!(program_state[1], 0);
+    }
+    let input = 8;
+    let mut program_state = [1108,input,8,1,4,1,99];
+    run_program(&mut program_state);
+    assert_eq!(program_state[1], 1);
+    for input in 9..12 {
+        let mut program_state = [1108,input,8,1,4,1,99];
+        run_program(&mut program_state);
+        assert_eq!(program_state[1], 0);
+    }
+}
+
+#[test]
+fn program_with_less_than_in_position_mode() {
+    for input in 0..8 {
+        let mut program_state = [7,7,8,7,4,7,99,input,8];
+        run_program(&mut program_state);
+        assert_eq!(program_state[7], 1);
+    }
+    for input in 8..12 {
+        let mut program_state = [7,7,8,7,4,7,99,input,8];
+        run_program(&mut program_state);
+        assert_eq!(program_state[7], 0);
+    }
+}
+
+#[test]
+fn program_with_equals_in_position_mode() {
+    for input in 0..8 {
+        let mut program_state = [8,7,8,7,4,7,99,input,8];
+        run_program(&mut program_state);
+        assert_eq!(program_state[7], 0);
+    }
+    let input = 8;
+    let mut program_state = [8,7,8,7,4,7,99,input,8];
+    run_program(&mut program_state);
+    assert_eq!(program_state[7], 1);
+    for input in 9..12 {
+        let mut program_state = [8,7,8,7,4,7,99,input,8];
+        run_program(&mut program_state);
+        assert_eq!(program_state[7], 0);
+    }
+}
+
+#[test]
+fn program_with_jump_in_position_mode() {
+    let input = 0;
+    let mut program_state = [6,10,13,1,11,12,11,4,11,99,input,0,1,9];
+    run_program(&mut program_state);
+    assert_eq!(program_state[11], 0);
+    let input = 3;
+    let mut program_state = [6,10,13,1,11,12,11,4,11,99,input,0,1,9];
+    run_program(&mut program_state);
+    assert_eq!(program_state[11], 1);
 }
 
 #[test]
@@ -289,6 +454,37 @@ fn parse_instruction_output() {
     let instruction = 4;
     assert_eq!(parse_instruction(instruction).0, Opcode::Output);
     assert_eq!(parse_instruction(instruction).1, ParameterMode::Position);
+}
+
+#[test]
+fn parse_instruction_jump_if_true() {
+    let instruction = 5;
+    assert_eq!(parse_instruction(instruction).0, Opcode::JumpIfTrue);
+    assert_eq!(parse_instruction(instruction).1, ParameterMode::Position);
+    assert_eq!(parse_instruction(instruction).2, ParameterMode::Position);
+    let instruction = 1105;
+    assert_eq!(parse_instruction(instruction).0, Opcode::JumpIfTrue);
+    // parse_instruction doesn't check whether given mode is valid
+    assert_eq!(parse_instruction(instruction).1, ParameterMode::Immediate);
+    assert_eq!(parse_instruction(instruction).2, ParameterMode::Immediate);
+}
+
+#[test]
+fn parse_instruction_jump_if_false() {
+    let instruction = 6;
+    assert_eq!(parse_instruction(instruction).0, Opcode::JumpIfFalse);
+}
+
+#[test]
+fn parse_instruction_less_than() {
+    let instruction = 107;
+    assert_eq!(parse_instruction(instruction).0, Opcode::LessThan);
+}
+
+#[test]
+fn parse_instruction_equals() {
+    let instruction = 8;
+    assert_eq!(parse_instruction(instruction).0, Opcode::Equals);
 }
 
 #[test]
