a24fc7c65ceece8f192d6b1cbcaf2139518b86bf
Cakemix <mholoch@gmail.com>
2020-01-03 12:18:20 +0100

    cleanup for part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,6 +1,5 @@
-use std::fs::read_to_string;
 use std::collections::HashSet;
-use std::convert::TryInto;
+use std::fs::read_to_string;
 
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum Space {
@@ -49,7 +48,8 @@ pub struct Map {
 }
 
 impl Direction {
-    fn norm(&self) -> Direction {
+    /// Returns the normalized vector and a usize that contains the value with which normlization took place
+    fn norm(&self) -> (Direction, usize) {
         let mut bigger_value = if self.x > self.y { self.x } else { self.y };
         let mut smaller_value = if self.x < self.y { self.x } else { self.y };
         while smaller_value != 0 {
@@ -60,42 +60,27 @@ impl Direction {
         if bigger_value < 0 {
             bigger_value *= -1;
         }
-        return Direction {
-            x: self.x / bigger_value,
-            y: self.y / bigger_value,
-        };
-    }
-}
-
-impl Iterator for Ray {
-    type Item = Coordinate;
-    fn next(&mut self) -> Option<Coordinate> {
-        let scaled_direction = Direction {
-            x: self.direction.x * self.step_count as i32,
-            y: self.direction.y * self.step_count as i32,
-        };
-        self.step_count += 1;
-        return Some(Coordinate {
-            x: (self.start.x as i32 + scaled_direction.x)
-                .try_into()
-                .expect("invalid x coordinate"),
-            y: (self.start.y as i32 + scaled_direction.y)
-                .try_into()
-                .expect("invalid y coordinate"),
-        });
+        return (
+            Direction {
+                x: self.x / bigger_value,
+                y: self.y / bigger_value,
+            },
+            bigger_value as usize,
+        );
     }
 }
 
 impl Coordinate {
-    fn cast_ray(&self, target: &Coordinate) -> Ray {
+    fn ray_to(&self, target: &Coordinate) -> Ray {
+        let vector_to_target = Direction {
+            x: target.x as i32 - self.x as i32,
+            y: target.y as i32 - self.y as i32,
+        };
+        let normalized_vector_to_target = vector_to_target.norm();
         Ray {
             start: self.clone(),
-            direction: Direction {
-                x: target.x as i32 - self.x as i32,
-                y: target.y as i32 - self.y as i32,
-            }
-            .norm(),
-            step_count: 1,
+            direction: normalized_vector_to_target.0,
+            step_count: normalized_vector_to_target.1,
         }
     }
 }
@@ -153,7 +138,7 @@ impl Map {
 
     pub fn get_best_asteroid(&self) -> (usize, Coordinate) {
         let mut max_count = 0;
-        let mut max_count_asteroid = &Coordinate{x: 0, y: 0};
+        let mut max_count_asteroid = &Coordinate { x: 0, y: 0 };
         for asteroid in self.asteroids.iter() {
             let count = self.count_visible_asteroids(&asteroid);
             if count > max_count {
@@ -167,9 +152,10 @@ impl Map {
     fn count_visible_asteroids(&self, at: &Coordinate) -> usize {
         let mut unique_ray_directions = HashSet::new();
         for asteroid in self.asteroids.iter() {
+            // don't include a ray to itself
             if asteroid != at {
-                let ray = at.cast_ray(&asteroid);
-                unique_ray_directions.insert(ray.direction.clone());
+                let ray = at.ray_to(&asteroid);
+                unique_ray_directions.insert(ray.direction.norm());
             }
         }
         return unique_ray_directions.len();
@@ -179,30 +165,33 @@ impl Map {
 fn main() {
     let map_string = read_to_string("input.txt").unwrap();
     let map = Map::from_str(&map_string);
-    println!("Got map\n{}",map.to_string());
+    println!("Got map\n{}", map.to_string());
     let (visible_asteroids_count, best_asteroid) = map.get_best_asteroid();
-    println!("Best asteroid at {:?} can observe {} other asteroids.", best_asteroid, visible_asteroids_count);
+    println!(
+        "Best asteroid at {:?} can observe {} other asteroids.",
+        best_asteroid, visible_asteroids_count
+    );
 }
 
 #[test]
 fn direction_normalization() {
     let ray_with_prime = Direction { x: 21, y: 5 };
-    assert_eq!(ray_with_prime, ray_with_prime.norm());
+    assert_eq!(ray_with_prime, ray_with_prime.norm().0);
     let ray_all_positive = Direction { x: 420, y: 10 };
-    assert_eq!(ray_all_positive.norm(), Direction { x: 42, y: 1 });
+    assert_eq!(ray_all_positive.norm().0, Direction { x: 42, y: 1 });
     let ray_all_positive_big_y = Direction { x: 42, y: 13818 };
-    assert_eq!(ray_all_positive_big_y.norm(), Direction { x: 1, y: 329 });
+    assert_eq!(ray_all_positive_big_y.norm().0, Direction { x: 1, y: 329 });
     let ray_all_positive_both_divisible = Direction { x: 34902, y: 13818 };
     assert_eq!(
-        ray_all_positive_both_divisible.norm(),
+        ray_all_positive_both_divisible.norm().0,
         Direction { x: 831, y: 329 }
     );
     let ray_partial_negative_x = Direction { x: -420, y: 10 };
-    assert_eq!(ray_partial_negative_x.norm(), Direction { x: -42, y: 1 });
+    assert_eq!(ray_partial_negative_x.norm().0, Direction { x: -42, y: 1 });
     let ray_partial_negative_y = Direction { x: 420, y: -10 };
-    assert_eq!(ray_partial_negative_y.norm(), Direction { x: 42, y: -1 });
+    assert_eq!(ray_partial_negative_y.norm().0, Direction { x: 42, y: -1 });
     let ray_all_negative = Direction { x: -420, y: -10 };
-    assert_eq!(ray_all_negative.norm(), Direction { x: -42, y: -1 });
+    assert_eq!(ray_all_negative.norm().0, Direction { x: -42, y: -1 });
 }
 
 #[test]
@@ -243,4 +232,18 @@ fn space_to_char_conversion() {
     let asteroid = Space::Asteroid;
     assert_eq!(empty_space.to_char(), '.');
     assert_eq!(asteroid.to_char(), '#');
+}
+
+#[test]
+fn part_one_works() {
+    let map_string = read_to_string("input.txt").unwrap();
+    let map = Map::from_str(&map_string);
+    println!("Got map\n{}", map.to_string());
+    let (visible_asteroids_count, best_asteroid) = map.get_best_asteroid();
+    println!(
+        "Best asteroid at {:?} can observe {} other asteroids.",
+        best_asteroid, visible_asteroids_count
+    );
+    assert_eq!(best_asteroid, Coordinate { x: 27, y: 19 });
+    assert_eq!(visible_asteroids_count, 314);
 }
